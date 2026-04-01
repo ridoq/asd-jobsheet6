7caa4e6560e2c2cd3a2f9f0f9eeb676611ae98ef
ridoq <ridoq444@gmail.com>
2026-04-01 11:03:11 +0700

    Praktikum 5.2

---
+++ Praktikum05/SortingMain23.java
@@ -0,0 +1,28 @@
+package Praktikum05;
+public class SortingMain23 {
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+        Sorting23 dataurut1 = new Sorting23(a, a.length);
+        System.out.println("Data awal 1");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut1.tampil();
+
+        int b[] = {30, 20, 2, 8, 14};
+        Sorting23 dataurut2 = new Sorting23(b, b.length);
+        System.out.println("\nData awal 2");
+        dataurut2.tampil();
+        dataurut2.selectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
+
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting23 dataurut3 = new Sorting23(c, c.length);
+        System.out.println("\nData awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
+    }
+}
